f6c7ae37f97cf55e6d8f295f1467753877440900
nosremetnarg <grant.alex.emerson@gmail.com>
2020-08-13 09:16:51 -0500

    server set up

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+// router.get('/', (req, res) => {
+
+// });
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
